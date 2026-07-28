eaa8ebe5348f04c967887e70fe9fd57e15362624
Saishiva-Anantha <saishiva.anantha117@gmail.com>
2026-07-28 21:42:39 +0530

    Add subtraction.py script for calculator

---
+++ subtraction.py
@@ -0,0 +1,12 @@
+def sub(a,b):
+    return a - b
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+result = sub(num1,num2)
+print("The difference is ",result)
+
+
+
+
+
