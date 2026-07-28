cf96536f42968070d88fc183c7758126346e4a93
Saishiva-Anantha <saishiva.anantha117@gmail.com>
2026-07-29 00:04:51 +0530

    feat: implement main application logic for calculator

--- subtraction.py
+++ subtraction.py
@@ -1,10 +1,7 @@
 def sub(a,b):
     return a - b
-num1 = int(input("Enter first number:"))
-num2 = int(input("Enter second number:"))
 
-result = sub(num1,num2)
-print("The difference is ",result)
+
 
 
 
